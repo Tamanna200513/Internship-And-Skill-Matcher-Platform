31a9058aad8a6f7f51b0908965327e7d4143396d
Archie Verma <varchieverma@gmail.com>
2026-02-11 21:55:53 +0530

    change

--- components/Common/Footer.jsx
+++ components/Common/Footer.jsx
@@ -1,6 +1,30 @@
+// components/BottomStats.jsx
+"use client";
+import React from "react";
 
-'use client';
+export default function BottomStats() {
+  return (
+    <section className="bg-[#e6eeff] py-30 flex justify-center">
+      
+      <div className="bg-white shadow-xl rounded-5xl px-16 py-20 
+                      flex flex-col md:flex-row gap-56 text-center ">
+        
+        <div>
+          <h2 className="text-4xl font-extrabold text-blue-800">5,000+</h2>
+          <p className="text-gray-500 mt-4">Internships Listed</p>
+        </div>
 
-export default function Footer() {
-  return <div>Footer</div>;
+        <div>
+          <h2 className="text-4xl font-extrabold text-blue-800">10,000+</h2>
+          <p className="text-gray-500 mt-2">Students Placed</p>
+        </div>
+
+        <div>
+          <h2 className="text-4xl font-extrabold text-blue-800">500+</h2>
+          <p className="text-gray-500 mt-2">Companies Partnered</p>
+        </div>
+
+      </div>
+    </section>
+  );
 }
